a78c812f04156eae669678fb5734432444291bde
Wahib Kerkeni <mr.wahib@gmail.com>
2022-06-29 11:52:30 +0100

    Added XML Configuration Example

--- src/main/java/mrw007/springframework/spring5jokesapp/Spring5JokesAppApplication.java
+++ src/main/java/mrw007/springframework/spring5jokesapp/Spring5JokesAppApplication.java
@@ -2,12 +2,14 @@ package mrw007.springframework.spring5jokesapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class Spring5JokesAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Spring5JokesAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5JokesAppApplication.class, args);
+    }
 
 }
